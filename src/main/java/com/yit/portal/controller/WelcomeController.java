c8505870a17f4ae6368d45c0e1f6d8a8b3055b8f
Kriangsak Pornpantiva <kriangsak.po@yipintsoi.com>
2021-04-26 16:08:12 +0700

    update draft product

--- src/main/java/com/yit/portal/controller/WelcomeController.java
+++ src/main/java/com/yit/portal/controller/WelcomeController.java
@@ -7,6 +7,8 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,9 +31,45 @@ public class WelcomeController {
 	private String message = "Hello World";
 
 	@RequestMapping("/")
-	public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
-		return "{\"name\":\"Ball\", \"age\":31, \"city\":\"Bangkok\"}";
+	public List<Product> welcome(Map<String, Object> model) {
+		List<Product> list = new ArrayList<Product>();
+		
+		Product p = new Product();
+		p.setId(1);
+		p.setDescription("RAM: 4, CPU: Intel Core i7-10750H");
+		p.setInstock(1);
+		p.setName("Notebook DELL");
+		p.setInstock(10);
+		
+		Product p2 = new Product();
+		p2.setId(2);
+		p2.setDescription("Smart TV TU7000");
+		p2.setName("SAMSUNG Crystal UHD 4K");
+		p2.setInstock(5);
+		
+		Product p3 = new Product();
+		p3.setId(3);
+		p3.setDescription("14976 BTU, Inverter");
+		p3.setName("HAIER Air Conditioning");
+		p3.setInstock(2);
+		
+		list.add(p);
+		list.add(p2);
+		list.add(p3);
+		
+		
+		return list;
+	}
+	
+	@GetMapping("/getProduct/{id}")
+    public Product getById(@PathVariable int id) {
+		Product p = new Product();
+		p.setId(1);
+		p.setDescription("RAM: 4, CPU: Intel Core i7-10750H");
+		p.setInstock(1);
+		p.setName("Notebook DELL");
+		p.setInstock(10);
+		return p;
 	}
 
 	@RequestMapping( value = "/getProduct", method = RequestMethod.GET)
